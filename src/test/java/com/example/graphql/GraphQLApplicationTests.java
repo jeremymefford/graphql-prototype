b619e9058170f3746525c018163a0af07ef24c35
Jeremy Mefford <jeremymefford@gmail.com>
2017-08-07 22:10:59 -0600

    Start for tests

--- src/test/java/com/example/graphql/GraphQLApplicationTests.java
+++ src/test/java/com/example/graphql/GraphQLApplicationTests.java
@@ -1,16 +1,26 @@
 package com.example.graphql;
 
+import org.hamcrest.Matchers;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import javax.annotation.Resource;
+
+import static org.junit.Assert.assertThat;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class GraphQLApplicationTests {
 
+    @Resource
+    private ApplicationContext ctxt;
+
     @Test
     public void contextLoads() {
+        assertThat(ctxt, Matchers.notNullValue());
     }
 
 }
